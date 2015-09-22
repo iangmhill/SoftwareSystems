d77db05c9e2a776ff1cc448eaae012c8b2a8a5b0
Ian Hill <ian.hill@students.olin.edu>
2015-09-22 10:35:14 -0400

    finished regex problem

--- hw02/find_track.c
+++ hw02/find_track.c
@@ -8,6 +8,7 @@ License: Creative Commons Attribution-ShareAlike 3.0
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 #define NUM_TRACKS 5
 
@@ -38,7 +39,14 @@ void find_track(char search_for[])
 // Prints track number and title.
 void find_track_regex(char pattern[])
 {
-    // TODO: fill this in
+    int i;
+    regex_t regex;
+    regcomp(&regex, pattern, 0);
+    for (i=0; i<NUM_TRACKS; i++) {
+        if (!regexec(&regex, tracks[i], 0, NULL, 0)) {
+            printf("Track %i: '%s'\n", i, tracks[i]);
+        }
+    }
 }
 
 // Truncates the string at the first newline, if there is one.
@@ -59,7 +67,7 @@ int main (int argc, char *argv[])
     fgets(search_for, 80, stdin);
     rstrip(search_for);
 
-    find_track(search_for);
+    find_track_regex(search_for);
     //find_track_regex(search_for);
 
     return 0;
